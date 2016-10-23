a6592636525edb80ccb797b611628175c6250366
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-10-23 13:01:58 -0700

    Align the key values

--- src/Keys.js
+++ src/Keys.js
@@ -1,9 +1,9 @@
 const Keys = {
-  Right: 39,
-  Left: 37,
-  Down: 40,
-  Up: 38,
-  Space: 32,
+  Right:    39,
+  Left:     37,
+  Down:     40,
+  Up:       38,
+  Space:    32,
   Letter_s: 83,
 };
 
