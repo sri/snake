31e6794406a2d205392cde8961c1fd5d4a27b203
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-10-24 01:36:43 -0700

    Add jest and a small test for it

---
+++ __tests__/Food-test.js
@@ -0,0 +1,9 @@
+test('food gets placed correctly', () => {
+  const Food = require('../src/Food.js').default;
+
+  const food = new Food();
+  food.place(10, 10);
+
+  expect(food.x).not.toBe(null);
+  expect(food.y).not.toBe(null);
+});
